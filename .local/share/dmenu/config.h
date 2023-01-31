5395a2f617f3511fd4d047dc4d1c28ba0b033716
ixp123 <77308732+ixp123@users.noreply.github.com>
2023-01-31 00:36:48 +0000

    Add files via upload

---
+++ .local/share/dmenu/config.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Hack:size=8",
+    "Hack Nerd Font:size=11"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#000000" },
+	[SchemeSel] = { "#ffffff", "#080808" },
+    [SchemeSelHighlight] = { "#de935f", "#080808" },
+    [SchemeNormHighlight] = { "#de935f", "#000000" },
+	[SchemeOut] = { "#ffffff", "#000000" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char startpipe[] = "#";
+static const char worddelimiters[] = " ";
