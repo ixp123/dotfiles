ddab332568d12fe352ffd754e30280b0dd3d7c89
ixp123 <77308732+ixp123@users.noreply.github.com>
2023-02-01 01:30:48 +0000

    Update blocks.h

--- .local/share/dwmblocks/blocks.h
+++ .local/share/dwmblocks/blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "/home/beau/.config/dwmblocks/scripts/mpdsong",	1,		0},
-    {"墳 ",  "/home/beau/.config/dwmblocks/scripts/volume",     1,      0},
-    {" ",  "/home/beau/.config/dwmblocks/scripts/clock",      1,      0},
+	{" ", "/home/beau/.local/share/dwmblocks/scripts/mpdsong",	1,		0},
+    {"墳 ",  "/home/beau/.local/share/dwmblocks/scripts/volume",     1,      0},
+    {" ",  "/home/beau/.local/share/dwmblocks/scripts/clock",      1,      0},
 
 };
 
